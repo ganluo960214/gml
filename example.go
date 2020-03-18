89fa0ca3632c3ce89f28580da5199dd03c4c11a0
gl <47106040@qq.com>
2020-03-18 23:09:25 +0800

    update example

--- example.go
+++ example.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate gml -type=example -file-name=example_gml.go
+//go:generate gml -type=example -file-name=u_can_set_file_name_or_by_default__-type_gml.go
 type example byte
 
 const (
